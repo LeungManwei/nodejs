47527a28fca575610fc8e4c512755fb9120608ea
Manwei <mic.liangwenwei@live.com>
2016-05-26 12:27:25 +0800

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ http.createServer(function(request, response) {
     var time = date.getTime();
     console.log("New request arrived."+time);
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hi, you are visiting " + os.hostname() + time.toString() + "\n");
+    response.write("Hi, you are visiting " + os.hostname() +" at: "+ time.toString() + "\n");
     response.end();
 }).listen(80);
 console.log("Server is running, listening on port 80....");
